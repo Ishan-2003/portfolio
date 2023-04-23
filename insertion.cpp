3fadfcf297b8c2da139605c7e18f2e0ddab93dda
Ishan-2003 <21BCS022@nith.ac.in>
2023-04-23 14:05:51 +0530

    my portfolio

---
+++ insertion.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    vector<int> v;
+    int data;
+    while (data!=-1)
+    {
+        cin>>data;
+        if(data!=-1){
+            v.push_back(data);
+        }
+    }
+    
+    
+    return 0;
+}
